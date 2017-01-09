3d30361dd33ad334ac4046bc33fdb5fa9c105210
halofreek45 <halofreek45@gmail.com>
2017-01-08 22:25:23 -0800

    Using GS Svg plugin

--- animation.js
+++ animation.js
@@ -2,6 +2,7 @@ var startXPos = 100;
 var startYPos = 50;
 var drawing = false;
 var letterCount = 0;
+var xOffSet;
 var availableLetters = {
     "A": true,
     "B": true,
@@ -32,27 +33,38 @@ var availableLetters = {
 }
 
 document.addEventListener("keyup", function(e) {
-  if(availableLetters[String.fromCharCode(e.keyCode).toUpperCase()]) {
-    drawing = true;
-    var pressedLetter = $('svg' + String.fromCharCode(e.keyCode).toUpperCase());
-    var clonedLetter = $('svg#svg' + String.fromCharCode(e.keyCode).toUpperCase()).clone();
-    clonedLetter.attr('id', 'current' + letterCount);
-    $('body').append(clonedLetter);
-
-    new Vivus('current' + letterCount , {
-        duration: 100
-    }, function() {
-        $(function() {
-          console.log("Started new Vivus");
-            jQuery.Color.hook("fill");
-            $('#current' + letterCount).children().find('#letter' + String.fromCharCode(e.keyCode).toUpperCase()).toggleClass('filler', 1000)
-            console.log(letterCount)
-            letterCount++;
-        })
-    });
-  }
-  else {
-    console.log("Letter is not Available")
-    console.log(drawing)
-  }
+  // if(availableLetters[String.fromCharCode(e.keyCode).toUpperCase()]) {
+  //   drawing = true;
+  //   var pressedLetter = $('svg' + String.fromCharCode(e.keyCode).toUpperCase());
+  //   var clonedLetter = $('svg#svg' + String.fromCharCode(e.keyCode).toUpperCase()).clone();
+  //   clonedLetter.attr('id', 'current' + letterCount);
+  //   $('body').append(clonedLetter);
+  //
+  //   new Vivus('current' + letterCount , {
+  //       duration: 100
+  //   }, function() {
+  //       $(function() {
+  //         console.log("Started new Vivus");
+  //           jQuery.Color.hook("fill");
+  //
+  //           console.log(letterCount)
+  //
+  //       })
+  //   });
+  //   letterCount++;
+  // }
+  // else {
+  //   console.log("Letter is not Available")
+  //   console.log(drawing)
+  // }
+  var currentLetter = $('#' + String.fromCharCode(e.keyCode).toUpperCase()).clone();
+  currentLetter.attr('id', 'current' + letterCount);
+  letterCount++;
+  xOffSet = (letterCount * 36);
+  currentLetter.attr('stroke', 'red')
+  currentLetter.attr('transform', 'translate(' + xOffSet + ', 5)');
+  $('#mainCanvas').append(currentLetter);
+  currentLetter.children('path').each(function() {
+    TweenLite.from(this , 2, {drawSVG: "50% 50%", delay: 0});
+  });
 })
